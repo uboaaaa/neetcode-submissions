12edd9d780299f39ee90bcdb07187c37e03de946
Minhal Khan <79323104+uboaaaa@users.noreply.github.com>
2026-07-08 14:46:29 -0400

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = [] 
+        part = []
+
+        def dfs(idx):
+            if idx == len(s):
+                res.append(part[:])
+                return 
+
+            for end in range(idx + 1, len(s) + 1):
+                chunk = s[idx:end]
+                if chunk == chunk[::-1]:
+                    part.append(chunk)
+                    dfs(end)
+                    part.pop()
+        
+        dfs(0)
+        return res
+
