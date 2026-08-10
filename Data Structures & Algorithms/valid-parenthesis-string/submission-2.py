ebf55b632c806c5050634501c8b750da5d5cf10a
Minhal Khan <79323104+uboaaaa@users.noreply.github.com>
2026-08-10 16:00:04 -0400

    Add: valid-parenthesis-string - submission-2

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        low_open_paren, high_open_paren = 0, 0
+        # ((**)
+        # 
+        for i, c in enumerate(s):
+            if c == '(':
+                low_open_paren += 1
+                high_open_paren += 1
+
+            if c == ')':
+                low_open_paren -= 1
+                high_open_paren -= 1
+
+            if c == '*': # star case
+                low_open_paren -= 1
+                high_open_paren += 1
+            
+            if low_open_paren < 0:
+                low_open_paren = 0
+                
+            if high_open_paren < 0:
+                return False
+            
+            print(f'at step {i}: {[low_open_paren, high_open_paren]}')
+
+        
+        return True if (low_open_paren == 0) else False
+                
